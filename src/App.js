49cd53343cc23d66965e803d8057bba7e15e039c
yogesh gaur <yogi7.hills@gmail.com>
2017-07-17 20:07:04 -0500

    add redux

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Test from './Test.js'
+import {connect} from 'react-redux';
 
 class App extends Component {
   render() {
@@ -11,11 +12,20 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <Test name="Gaur Associates"/>
-         
+        <Test name="Gaur Associates" />
+        <p>
+          Counter = {this.props.counter}
+        </p>
       </div>
+
     );
   }
 }
 
-export default App;
+function mapStateToProps(state){
+    return {
+        counter : state.cnt
+    }
+}
+
+export default connect(mapStateToProps)(App);
