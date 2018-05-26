abeb674dfa862cbe3bf70ef31e4ab0c7c5abb673
yogesh gaur <yogi7.hills@gmail.com>
2018-05-25 19:16:21 -0500

    react redux devtools

--- src/index.js
+++ src/index.js
@@ -4,13 +4,17 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { createLogger } from 'redux-logger';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware ,  compose} from 'redux';
 import { Provider } from 'react-redux';
 import allReducers from './reducers.js'
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import thunk from 'redux-thunk';
 
-const logger = createLogger();
-const store = createStore(allReducers, applyMiddleware(logger));
+//const logger = createLogger();
+//const store = createStore(allReducers, applyMiddleware(logger));
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(allReducers,composeEnhancers( applyMiddleware(  thunk)));
 
 store.dispatch({ type: "ADD_CNT", payload: 5 });
 store.dispatch({ type: "ADD_CNT", payload: 10 });
