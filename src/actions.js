104b7039728083e31d9f7dbefd09f2d50dd53a0e
yogesh gaur <yogi7.hills@gmail.com>
2018-05-29 19:47:58 -0500

    redux thunk

--- src/actions.js
+++ src/actions.js
@@ -5,9 +5,21 @@ export  function incCounter() {
     }
 }
 
-export  function userName(name) {
+export  function nonThunkUserName(name) {
     return {
         type: "LAST_USER",
         payload: name
     }
+}
+
+export  function userName(name) {
+    return (dispatch, getState) => {
+        
+        console.log(getState());
+
+        fetch('https://www.google.com', {
+            mode: 'no-cors'
+        }). then (dispatch(nonThunkUserName(name)));
+    }
+
 }
