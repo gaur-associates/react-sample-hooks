9f727f5d6893d5a7336f425a0d29429a630caf56
yogesh gaur <yogi7.hills@gmail.com>
2018-06-16 08:17:13 -0500

    react tabs

--- src/routes/Posts.js
+++ src/routes/Posts.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Axios from 'axios';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 
 class Posts extends Component {
 
@@ -31,9 +33,24 @@ class Posts extends Component {
     }
 
     render() {
-        const displayPosts = this.state.posts.map((x) => {
-            return (<p key={x.id}> {x.title} </p>)
+        // const displayPosts = this.state.posts.map((x) => {
+        //     return (<p key={x.id}> {x.title} </p>)
+        // });
+        const title = this.state.posts.map((x) => {
+            return (<Tab key={x.id}> {x.id} </Tab>)
         });
+        const text = this.state.posts.map((x) => {
+            return (<TabPanel key={x.id}> {x.title} </TabPanel>)
+        });
+
+        const displayPosts = (
+            <Tabs defaultIndex={1} onSelect={index => console.log(index)}>
+                <TabList>
+                    {title}
+                </TabList>
+                {text}
+            </Tabs>
+        );
 
         return (
             <div>
