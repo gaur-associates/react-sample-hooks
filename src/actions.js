57467bf3a5f2a6348b53063990b9e2f98a1002c4
yogesh gaur <yogi7.hills@gmail.com>
2019-06-02 16:29:41 -0500

    pretty code

--- src/actions.js
+++ src/actions.js
@@ -1,25 +1,23 @@
-export  function incCounter() {
-    return {
-        type: "ADD_CNT",
-        payload: 1
-    }
+export function incCounter() {
+  return {
+    type: "ADD_CNT",
+    payload: 1
+  };
 }
 
-export  function nonThunkUserName(name) {
-    return {
-        type: "LAST_USER",
-        payload: name
-    }
+export function nonThunkUserName(name) {
+  return {
+    type: "LAST_USER",
+    payload: name
+  };
 }
 
-export  function userName(name) {
-    return (dispatch, getState) => {
-        
-        console.log(getState());
+export function userName(name) {
+  return (dispatch, getState) => {
+    console.log(getState());
 
-        fetch('https://www.google.com', {
-            mode: 'no-cors'
-        }). then (dispatch(nonThunkUserName(name)));
-    }
-
+    fetch("https://www.google.com", {
+      mode: "no-cors"
+    }).then(dispatch(nonThunkUserName(name)));
+  };
 }
