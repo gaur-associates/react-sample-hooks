93990073083402707d43c6cdcd171be64f1547a7
yogesh gaur <yogi7.hills@gmail.com>
2019-06-02 16:47:05 -0500

    use react hooks -> useState, useEffect

--- src/routes/Posts.js
+++ src/routes/Posts.js
@@ -1,63 +1,53 @@
-import React, { Component } from 'react';
-import Axios from 'axios';
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-import 'react-tabs/style/react-tabs.css';
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
 
-class Posts extends Component {
+const Posts = () => {
+  const [posts, setPosts] = useState([]);
 
-    constructor() {
-        super();
-        this.state = {
-            posts: []
-        };
-    }
+  useEffect(() => {
+    Axios.get("https://jsonplaceholder.typicode.com/posts")
+      .then(response => {
+        let posts = response.data.slice(0, 5);
+        setPosts(posts);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+    return () => {
+      console.log("component unmount");
+    };
+  }, []);
 
-    componentDidMount() {
-        Axios.get("https://jsonplaceholder.typicode.com/posts")
-            .then(response => {
-                let posts = response.data.slice(0, 5);
-                this.setState({
-                    posts: posts
-                })
-            })
-            .catch(error => {
-                console.error(error);
-            })
-    }
+  const h2style = {
+    color: "blue",
+    fontSize: "x-large",
+    textAlign: "center"
+  };
 
-    h2style = {
-        color: 'blue',
-        fontSize: 'x-large',
-        textAlign: 'center',
+  // const displayPosts = this.state.posts.map((x) => {
+  //     return (<p key={x.id}> {x.title} </p>)
+  // });
+  const title = posts.map(x => {
+    return <Tab key={x.id}> {x.id} </Tab>;
+  });
+  const text = posts.map(x => {
+    return <TabPanel key={x.id}> {x.title} </TabPanel>;
+  });
 
-    }
+  const displayPosts = (
+    <Tabs defaultIndex={1} onSelect={index => console.log(index)}>
+      <TabList>{title}</TabList>
+      {text}
+    </Tabs>
+  );
 
-    render() {
-        // const displayPosts = this.state.posts.map((x) => {
-        //     return (<p key={x.id}> {x.title} </p>)
-        // });
-        const title = this.state.posts.map((x) => {
-            return (<Tab key={x.id}> {x.id} </Tab>)
-        });
-        const text = this.state.posts.map((x) => {
-            return (<TabPanel key={x.id}> {x.title} </TabPanel>)
-        });
-
-        const displayPosts = (
-            <Tabs defaultIndex={1} onSelect={index => console.log(index)}>
-                <TabList>
-                    {title}
-                </TabList>
-                {text}
-            </Tabs>
-        );
-
-        return (
-            <div>
-                <h2 style={this.h2style}>About Gaur Associates</h2>
-                {displayPosts}
-            </div>
-        );
-    }
-}
+  return (
+    <div>
+      <h2 style={h2style}>About Gaur Associates</h2>
+      {displayPosts}
+    </div>
+  );
+};
 export default Posts;
